7a39a030e038bd3e4e7e7441e68e45c7665d2069
Mariano Cavallo <mariano.cavallo@gmail.com>
2020-08-15 00:42:54 +0200

    Updates

---
+++ component---src-pages-index-js-9b2352703a548d5934a3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{RXBc:function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return r}));var u=e("q1tI"),c=e.n(u);function r(){return c.a.createElement("div",null,"👋")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-9b2352703a548d5934a3.js.map
